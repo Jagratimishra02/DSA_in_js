bd3a8103c94eb6075dea75154fbb86e88683a282
Jagratimishra02 <nishimisra002@gmail.com>
2026-03-28 10:03:20 +0000

    basics done

---
+++ basic.js
@@ -0,0 +1,39 @@
+// sum of two numbers
+
+let a = 10;
+let b = 20;
+console.log(a+b);     // addition 
+
+ // if the answer in console is in blue colour it is number,if it is in black/white(if pc has dark mode) then it is string
+
+
+ let c = 10;
+ let d = "20";
+console.log(c+d);
+console.log(typeof(c+d));
+
+// addition of string + number is known as "concatination", add of two different datatypes is known as concatination
+
+
+
+
+// ++++++++++++++++++++++++++ relation between string nd integer +++++++++++++++++++++++++++++++++++++++++++++++++
+
+ a = 13;
+ b = 10;
+console.log("sum of a and b is :" + (a+b));     // output is string  , string + num => string
+
+
+
+
+//+++++++++++++++++ TYPE COERCION ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(1+"1");  // output is string bcoz of concatination
+
+console.log(1-"1");    // op is num
+console.log(1*"1");     //op is num
+console.log(1/"1");     //op is num
+console.log(1%"1");     // op is num
+
+
